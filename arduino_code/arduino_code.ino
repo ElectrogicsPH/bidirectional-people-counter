acde976e532d28e9ea9caffceb43a28615256cee
jepoy <jefersondeleonjepoy@gmail.com>
2023-06-18 00:58:21 +0800

    rename variables

--- arduino_code/arduino_code.ino
+++ arduino_code/arduino_code.ino
@@ -3,15 +3,15 @@ const int sensor_1_pin = 2; // Sensor 1 input pin
 const int sensor_2_pin = 3; // Sensor 2 input pin
 
 // Variables to store people count
-int peopleCount = 0;
+int people_count = 0;
 
 // Variables to track sensor states
-bool sensor1State = false;
-bool sensor2State = false;
+bool sensor_1_state = false;
+bool sensor_2_state = false;
 
 // Previous state variables to debounce the sensors
-bool prevSensor1State = false;
-bool prevSensor2State = false;
+bool prevsensor_1_state = false;
+bool prevsensor_2_state = false;
 
 const int RESET_TIME_THRESHOLD = 5000;
 const int READ_SENSOR_TIME_INTERVAL = 100;
@@ -49,19 +49,19 @@ void loop()
 void people_count(void)
 {
     // Read sensor states
-    sensor1State = digitalRead(sensor_1_pin);
-    sensor2State = digitalRead(sensor_2_pin);
+    sensor_1_state = digitalRead(sensor_1_pin);
+    sensor_2_state = digitalRead(sensor_2_pin);
 
     // Handle system state transitions
     switch (currentState)
     {
     case IDLE:
-        if (sensor1State && !prevSensor1State)
+        if (sensor_1_state && !prevsensor_1_state)
         {
             currentState = ENTRY_IN_PROGRESS;
             stateStartTime = millis(); // Record the start time of the state
         }
-        else if (sensor2State && !prevSensor2State)
+        else if (sensor_2_state && !prevsensor_2_state)
         {
             currentState = EXIT_IN_PROGRESS;
             stateStartTime = millis(); // Record the start time of the state
@@ -69,12 +69,12 @@ void people_count(void)
         break;
 
     case ENTRY_IN_PROGRESS:
-        if (sensor2State && !prevSensor2State)
+        if (sensor_2_state && !prevsensor_2_state)
         {
-            peopleCount++;
+            people_count++;
             // Print current people count
             Serial.print("People Count: ");
-            Serial.println(peopleCount);
+            Serial.println(people_count);
             Serial.println("Someone entered the room");
             currentState = IDLE;
         }
@@ -85,12 +85,12 @@ void people_count(void)
         break;
 
     case EXIT_IN_PROGRESS:
-        if (sensor1State && !prevSensor1State)
+        if (sensor_1_state && !prevsensor_1_state)
         {
-            peopleCount--;
+            people_count--;
             // Print current people count
             Serial.print("People Count: ");
-            Serial.println(peopleCount);
+            Serial.println(people_count);
             Serial.println("Someone exited the room");
             currentState = IDLE;
         }
@@ -107,6 +107,6 @@ void people_count(void)
     }
 
     // Update previous sensor states
-    prevSensor1State = sensor1State;
-    prevSensor2State = sensor2State;
+    prevsensor_1_state = sensor_1_state;
+    prevsensor_2_state = sensor_2_state;
 }
